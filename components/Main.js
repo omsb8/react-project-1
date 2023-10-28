670824bfaad9588516c88d6b987e1cfedaee4454
Ousama Batais <sooomabatais@gmail.com>
2023-10-28 14:55:54 -0500

    first commit

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Some facts about Me</h1>
+            <ul className="main--facts">
+                <li>Often view failures as valuable learning experiences</li>
+                <li>Have talents and interests outside my primary field</li>
+                <li>Have a global outlook, valuing diversity and being open to different cultures and perspectives</li>
+                <li>Make time for family, friends, and leisure activities</li>
+                <li>Leverage technology to enhance my work and personal life</li>
+            </ul>
+        </main>
+    )
+}
